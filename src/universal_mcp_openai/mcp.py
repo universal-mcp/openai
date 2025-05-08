81cc85bd1092bb58be8745c65ce07709b2c7766a
LoNeWoLf26548 <arstejas7@gmail.com>
2025-05-08 14:44:37 +0530

    feat: Initial commit from MCP CLI init command

---
+++ src/universal_mcp_openai/mcp.py
@@ -0,0 +1,19 @@
+
+from universal_mcp.servers import SingleMCPServer
+from universal_mcp.integrations import ApiKeyIntegration
+from universal_mcp.stores import EnvironmentStore
+
+from universal_mcp_openai.app import OpenaiApp
+
+env_store = EnvironmentStore()
+integration_instance = ApiKeyIntegration(name="OPENAI_API_KEY", store=env_store)
+app_instance = OpenaiApp(integration=integration_instance)
+
+mcp = SingleMCPServer(
+    app_instance=app_instance,
+)
+
+if __name__ == "__main__":
+    mcp.run()
+
+
